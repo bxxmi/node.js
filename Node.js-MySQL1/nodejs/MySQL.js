fc606abfc82cee8cdd6e7da3c615dd8428185507
bxxmi <bmi6638@gmail.com>
2020-06-06 14:07:41 +0900

    install npm commit

---
+++ Node.js-MySQL1/nodejs/MySQL.js
@@ -0,0 +1,21 @@
+// mysql 모듈을 mysql변수에 담아 사용하겠다
+var mysql      = require('mysql');
+// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다.
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '6638',
+  database : 'opentutorials'
+});
+
+// 접속해라
+connection.connect();
+
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+
+connection.end();
