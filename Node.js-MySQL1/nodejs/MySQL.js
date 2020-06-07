241d475327accfd506707100faba4c6729b4dff5
bxxmi <bmi6638@gmail.com>
2020-06-07 14:47:35 +0900

    06/07 commit

--- Node.js-MySQL1/nodejs/MySQL.js
+++ Node.js-MySQL1/nodejs/MySQL.js
@@ -1,5 +1,5 @@
 // mysql 모듈을 mysql변수에 담아 사용하겠다
-var mysql      = require('mysql');
+var mysql = require('mysql');
 // 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다.
 var connection = mysql.createConnection({
   host     : 'localhost',
